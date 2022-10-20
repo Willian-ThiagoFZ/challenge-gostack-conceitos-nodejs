3dadf7e01e86a427072999dd2a6981d23020bfdf
willian <willianthiagofozz@hotmail.com>
2022-10-19 22:29:32 -0300

    create function to Store Repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4 } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,38 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json({
+    repositories: repositories
+  })
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  response.json(repository);
+  
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
 });
 
 module.exports = app;
